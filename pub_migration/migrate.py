3f41baca3646a0eca6b8d140c4909d670034f09e
wolearyc <wolearyc@gmail.com>
2019-01-05 19:26:27 -0500

    initial commit

---
+++ pub_migration/migrate.py
@@ -0,0 +1,23 @@
+f = open('info')
+links = open('links')
+for i in range(84): 
+    title = f.readline()
+    authors = f.readline()
+    pub_info = f.readline()
+    journal = pub_info.split(',',1)[0]
+    paper_spec = ',' +  pub_info.split(',',1)[1]
+    f.readline()
+    link = links.readline()
+    print(
+f"""
+<tr>
+  <th scope="row">{84-i}</th>
+  <td>
+    <a {link}>{title}</a><br>
+    {authors}<br>
+    <span class="font-weight-bold">{journal}</span>{paper_spec}
+  </td>
+</tr>""")
+
+
+
